51a598ccab31ea099673429aaa110f2f0142515e
scottcagno <scottiecagno@gmail.com>
2022-01-20 02:40:36 -0500

    [webapp] -- working on refining

--- pkg/webapp/server.go
+++ pkg/webapp/server.go
@@ -1,9 +1,39 @@
 package webapp
 
-import "net/http"
+import (
+	"net/http"
+)
 
-type Server struct {
-	*http.Server
-	SessionStore
-	AuthUser
+type ApplicationConfig struct {
+	SessionID string
+	Addr      string
+	Handler   http.Handler
+}
+
+func checkConf(conf *ApplicationConfig) {
+
+}
+
+type Application struct {
+	conf *ApplicationConfig
+	sess *SessionStore
+	auth *BasicAuthUser
+	serv *http.Server
+}
+
+func NewApplication(conf *ApplicationConfig) *Application {
+	checkConf(conf)
+	return &Application{
+		conf: conf,
+		sess: NewSessionStore(conf.SessionID, 900),
+		auth: NewBasicAuthUser(),
+		serv: &http.Server{
+			Addr:    conf.Addr,
+			Handler: conf.Handler,
+		},
+	}
+}
+
+func (app *Application) ListenAndServe() error {
+	return app.serv.ListenAndServe()
 }
